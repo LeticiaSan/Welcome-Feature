87bfabdf5ca960b293bd4ed33e7ffa2c963eff39
Letícia Santos <leticia.santos74@fatec.sp.gov.br>
2021-09-20 14:34:33 -0300

    Atualização

--- src/Welcome.java
+++ src/Welcome.java
@@ -8,28 +8,40 @@ public class Welcome {
 		Map<String, User> mapCpf = new HashMap<String, User>();
 		Map<String, User> mapEmail = new HashMap<String, User>();
 		
+		WelcomeServices welcomeServices = new WelcomeServices(mapCpf, mapEmail);
+		
 		User user1 = new User("123.456.789-01","alvaro@domain.com","Sr.","Álvaro","Magalhães");
 		User user2 = new User("123.456.779-01","joao@domain.com","Dr.","João","House");
-		User user3 = new User(null,"gerdadir@domain.com","Dr.","Gerdadir","Cavalcanti");
+		User user3 = new User("123.456.777-01","gerdadir@domain.com","Dr.","Gerdadir","Cavalcanti");
 		
-		mapCpf.put(WelcomeServices.turnCpfNumbersOnly(user1.cpf),user1);
-		mapCpf.put(WelcomeServices.turnCpfNumbersOnly(user2.cpf),user2);
-		mapCpf.put(WelcomeServices.turnCpfNumbersOnly(user3.cpf),user3);
+		welcomeServices.putCpf(user1);
+		welcomeServices.putCpf(user2);
+		welcomeServices.putCpf(user3);
 		
-		mapEmail.put(user1.email,user1);
-		mapEmail.put(user2.email,user2);
-		mapEmail.put(user3.email,user3);
+		welcomeServices.putEmail(user1);
+		welcomeServices.putEmail(user2);
+		welcomeServices.putEmail(user3);
 		
-		WelcomeServices.welcomeServices(mapCpf, mapEmail);		
+		System.out.println(welcomeServices.listarMap(welcomeServices.mapCpf)); 
+		
+//		mapCpf.put(WelcomeServices.turnCpfNumbersOnly(user1.cpf),user1);
+//		mapCpf.put(WelcomeServices.turnCpfNumbersOnly(user2.cpf),user2);
+//		mapCpf.put(WelcomeServices.turnCpfNumbersOnly(user3.cpf),user3);
+//		
+//		mapEmail.put(user1.email,user1);
+//		mapEmail.put(user2.email,user2);
+//		mapEmail.put(user3.email,user3);
+		
+		//WelcomeServices.welcomeServices(mapCpf, mapEmail);		
 		
 		try (Scanner sc = new Scanner(System.in)) {
 			while(true){
 				String searchVariable = sc.nextLine();
-				if(WelcomeServices.verifyCpf(searchVariable)) {
-					System.out.println(WelcomeServices.searchUserByCpf(WelcomeServices.turnCpfNumbersOnly(searchVariable)));
+				if(welcomeServices.verifyCpf(searchVariable)) {
+					System.out.println(welcomeServices.searchUserByCpf(welcomeServices.turnCpfNumbersOnly(searchVariable)));
 				}
-				else if(WelcomeServices.verifyEmailAdress(searchVariable)) {
-					System.out.println(WelcomeServices.searchUserByEmailAdress(searchVariable));
+				else if(welcomeServices.verifyEmailAdress(searchVariable)) {
+					System.out.println(welcomeServices.searchUserByEmailAdress(searchVariable));
 				}
 				else {
 					System.out.println("Usuário não encontrado");
--- src/WelcomeServices.java
+++ src/WelcomeServices.java
@@ -5,15 +5,27 @@ import java.util.regex.Pattern;
 
 public class WelcomeServices{
 	    
-		public static Map<String, User> mapCpf = new HashMap<String, User>();
-		public static Map<String, User> mapEmail = new HashMap<String, User>();
+		public Map<String, User> mapCpf = new HashMap<String, User>();
+		public Map<String, User> mapEmail = new HashMap<String, User>();
 		
-		public static void welcomeServices(Map<String, User> cpf, Map<String, User> email){
-			mapCpf = cpf;
-			mapEmail = email;
+		public WelcomeServices(Map<String, User> mapCpf, Map<String, User> mapEmail){
+			this.mapCpf = mapCpf;
+			this.mapEmail = mapEmail;
 		}
-					
-		public static boolean verifyByRegex(String input, String regex) {
+		
+		public void putCpf(User user) {
+			mapCpf.put(user.cpf, user);
+		}
+		
+		public String listarMap(Map<String, User> mapCpf) {
+			return mapCpf.toString();
+		}
+		
+		public void putEmail(User user) {
+			mapEmail.put(user.email, user);
+		}
+		
+		public boolean verifyByRegex(String input, String regex) {
 		    boolean isInputValid = false;
 		    if (input != null && input.isEmpty() == false) {
 		        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
@@ -25,15 +37,15 @@ public class WelcomeServices{
 		    return isInputValid;
 		}
 		
-		public static boolean verifyEmailAdress(String email) {
-			return WelcomeServices.verifyByRegex(email,"^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$");
+		public boolean verifyEmailAdress(String email) {
+			return verifyByRegex(email,"^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$");
 		}
 
-		public static boolean verifyCpf(String cpf) {
-			return WelcomeServices.verifyByRegex(cpf,"[0-9]{3}[\\\\.]?[0-9]{3}[\\\\.]?[0-9]{3}[-]?[0-9]{2}");
+		public boolean verifyCpf(String cpf) {
+			return verifyByRegex(cpf,"[0-9]{3}[\\\\.]?[0-9]{3}[\\\\.]?[0-9]{3}[-]?[0-9]{2}");
 		}
 				
-		public static String turnCpfNumbersOnly(String cpfOnlyNum) {
+		public String turnCpfNumbersOnly(String cpfOnlyNum) {
 			if(cpfOnlyNum != null)
 			{
 				return cpfOnlyNum.replace(".","").replace("-","").replace(" ", "");
@@ -41,15 +53,15 @@ public class WelcomeServices{
 			return "";
 		}
 		
-		public static String searchUserByCpf(String inputCpf){
+		public String searchUserByCpf(String inputCpf){
 			User user = mapCpf.get(inputCpf);
 			if(user != null) {
 				return user.greetUser(inputCpf);
 			}
-			return "Usuário não encontrado";
+			return "NULOUsuário não encontrado";
 		}
 		
-		public static String searchUserByEmailAdress(String inputEmail){
+		public String searchUserByEmailAdress(String inputEmail){
 			User user = mapEmail.get(inputEmail);
 			if(user != null) {
 				return user.greetUser(inputEmail);
@@ -57,5 +69,6 @@ public class WelcomeServices{
 			return "Usuário não encontrado";
 			
 		}
+		
 		
 }
