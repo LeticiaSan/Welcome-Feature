8dad34432643f57a637daa1109f17d9c20b5b1f3
Letícia Santos <leticia.santos74@fatec.sp.gov.br>
2021-09-21 16:03:50 -0300

    Change Request 1

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,20 +4,22 @@ public class Welcome {
 		
 	public static void main(String[] args) {
 		WelcomeServices welcomeServices = new WelcomeServices();
-		
+		String enableSearchByEmail = "";
+		welcomeServices.inputEnableEmail(enableSearchByEmail);
+
 		User user1 = new User("123.456.789-01","alvaro@domain.com","Sr.","Álvaro","Magalhães");
 		User user2 = new User("123.456.779-01","joao@domain.com","Dr.","João","House");
-		User user3 = new User("123.456.777-01","gerdadir@domain.com","Dr.","Gerdadir","Cavalcanti");
+		User user3 = new User("123.456.777-01","manuela@domain.com","Dra.","Manuela","Cavalcanti");
 		
 		welcomeServices.put(user1);
 		welcomeServices.put(user2);
 		welcomeServices.put(user3);
-			
+
 		try (Scanner sc = new Scanner(System.in)) {
 			while(true){
 				String searchVariable = sc.nextLine();
 				if(welcomeServices.verifyCpf(searchVariable)) {
-					System.out.println(welcomeServices.searchUserByCpf(welcomeServices.turnCpfNumbersOnly(searchVariable)));
+					System.out.println(welcomeServices.searchUserByCpf(searchVariable));
 				}
 				else if(welcomeServices.verifyEmailAdress(searchVariable)) {
 					System.out.println(welcomeServices.searchUserByEmailAdress(searchVariable));
--- src/WelcomeServices.java
+++ src/WelcomeServices.java
@@ -7,15 +7,24 @@ public class WelcomeServices{
 	    
 		public Map<String, User> mapCpf = new HashMap<String, User>();
 		public Map<String, User> mapEmail = new HashMap<String, User>();
-				
+		public boolean enableSearchByEmail;
+
+		public void inputEnableEmail(String s){
+			this.enableSearchByEmail = parseBoolean(s);
+		}
+
+		public static boolean parseBoolean(String s){
+			return s.equals("true") || s.equals("1");
+		}
+
 		public void put(User user) {
-			this.mapCpf.put(this.turnCpfNumbersOnly(user.cpf), user);
+			this.mapCpf.put(turnCpfNumbersOnly(user.cpf), user);
 			this.mapEmail.put(user.email, user);
 		}
 				
 		public boolean verifyByRegex(String input, String regex) {
 		    boolean isInputValid = false;
-		    if (input != null && input.isEmpty() == false) {
+		    if (input != null && !input.isEmpty()) {
 		        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 		        Matcher matcher = pattern.matcher(input);
 		        if (matcher.matches()) {
@@ -42,7 +51,7 @@ public class WelcomeServices{
 		}
 		
 		public String searchUserByCpf(String inputCpf){
-			User user = this.mapCpf.get(inputCpf);
+			User user = this.mapCpf.get(turnCpfNumbersOnly(inputCpf));
 			if(user != null) {
 				return user.greetUser(inputCpf);
 			}
@@ -51,7 +60,7 @@ public class WelcomeServices{
 		
 		public String searchUserByEmailAdress(String inputEmail){
 			User user = this.mapEmail.get(inputEmail);
-			if(user != null) {
+			if(user != null && enableSearchByEmail) {
 				return user.greetUser(inputEmail);
 			}
 			return "Usuário não encontrado";
