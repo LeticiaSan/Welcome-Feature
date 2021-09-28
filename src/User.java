1c021bde430970209a2d1a0c044450cdb3cb21fd
Letícia Santos <leticia.santos74@fatec.sp.gov.br>
2021-09-28 19:56:28 -0300

    US02 E CR02 - Ajustes

--- src/User.java
+++ src/User.java
@@ -1,7 +1,3 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Random;
-
 public class User {
 	public String cpf;
 	public String email;
@@ -10,10 +6,6 @@ public class User {
 	public String last_name;
  	public String style;
 
-	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH");
-	public int hourNow = Integer.parseInt(dtf.format(LocalDateTime.now()));
-	Random random = new Random();
-
 	public User(String cpf, String email, String title, String first_name, String last_name, String style){
 		this.cpf = cpf;
 		this.email = email;
@@ -21,38 +13,6 @@ public class User {
 		this.first_name = first_name;
 		this.last_name = last_name;
 		this.style = style;
-	}
-
-	public String greetUser(){
-		System.out.println("style: " + this.style);
-		return switch (this.style){
-			case "formal" -> greetType(1);
-			case "informal" -> greetType(2);
-			case "casual" -> greetType(3);
-			case "southtern" -> greetType(4);
-			case "random" -> greetType(random.nextInt(5));
-			default -> greetType(0);
-		};
-	}
-
-	public String greetType(int number){
-		return switch (number) {
-			case 1 -> formalGreetUser();
-			case 2 -> "Hi there, " + this.title + " " + this.first_name + " " + this.last_name;
-			case 3 -> "Yo, " + this.title + " " + this.first_name + " " + this.last_name;
-			case 4 -> "Howdy, " + this.title + " " + this.first_name + " " + this.last_name;
-			default -> "Hello, " + this.title + " " + this.first_name + " " + this.last_name;
-		};
-	}
-
-	public String formalGreetUser() {
-		if(hourNow > 5 && hourNow < 12) {
-			return "Good Morning, " + this.title + " " + this.first_name + " " + this.last_name;
-		}
-		if(hourNow < 18){
-			return "Good Afternoon, " + this.title + " " + this.first_name + " " + this.last_name;
-		}
-		return "Good Evening, " + this.title + " " + this.first_name + " " + this.last_name;
 	}
 
 	@Override
