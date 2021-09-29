b47f7cdefb013a8d85a98320232b9f601fb8957c
Letícia Santos <leticia.santos74@fatec.sp.gov.br>
2021-09-29 14:04:34 -0300

    US03 E CR02 - funcionando

--- src/Welcome.java
+++ src/Welcome.java
@@ -7,15 +7,18 @@ public class Welcome {
 		WelcomeServices welcomeServices = new WelcomeServices();
 		welcomeServices.inputEnableSearchByEmail("welcome.properties");
 		UserConstructor constructor = new UserConstructor();
+
 		User user1 = constructor.insertCpf("123.456.789-01").insertEmail("alvaro@domain.com").insertTitle("Sr.").insertFirst_name("Álvaro").insertLast_name("Magalhães").insertStyle("formal").construct();
-		User user2 = constructor.insertCpf("123.456.779-01").insertEmail("joao@domain.com").insertTitle("Dr.").insertFirst_name("João").insertLast_name("House").insertStyle("random").construct();
-		User user3 = constructor.insertCpf("123.456.777-01").insertEmail("manuela@domain.com").insertTitle("Dra.").insertFirst_name("Manuela").insertLast_name("Cavalcanti").insertStyle("").construct();
+		System.out.println(user1.toString());
+		welcomeServices.putUser(user1, constructor);
 
+		User user2 = constructor.insertCpf("123.456.779-01").insertEmail("joao@domain.com").insertTitle("Dr.").insertFirst_name("João").insertLast_name("House").insertStyle("casual").construct();
+		System.out.println(user2.toString());
+		welcomeServices.putUser(user2, constructor);
+
+		User user3 = constructor.insertCpf("123.456.777-01").insertEmail("manuela@domain.com").insertTitle("Dra.").insertFirst_name("Manuela").insertLast_name("Cavalcanti").construct();
 		System.out.println(user3.toString());
-
-		welcomeServices.putUser(user1);
-		welcomeServices.putUser(user2);
-		welcomeServices.putUser(user3);
+		welcomeServices.putUser(user3, constructor);
 
 		try (Scanner sc = new Scanner(System.in)) {
 			while(true){
